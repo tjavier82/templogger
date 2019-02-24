9ce3327053c1fffaec51b3a3c0be35f1d70c9358
tjavier82 <tjavier82@gmail.com>
2019-02-24 19:54:28 +0100

    Se añade la parte de OpenWeather y se sustituyen variables globales por entradas de un fichero de configuración.

--- templogger.py
+++ templogger.py
@@ -3,24 +3,34 @@
 import logging
 import serial
 import time
+import configparser
+import pyowm
 
-#Serial config
-SERIAL_PORT = '/dev/ttyACM0'
-SERIAL_BAUD_RATE = 9600
-SERIAL_TIMEOUT = 4
+# config file will have following sections
+# Serial
+#   SerialPort
+#   SerialBaudRate
+#   SerialTimeout
+#   SleepTime
+#
+# Output
+#   FilePath
+#
+# Logging
+#   LoggerName
+#
+# OpenWeather
+#   APIKey
+#   Lat
+#   Lon
 
-#File config
-PATH = '/home/pi/Dev/templogger/temp.csv'
 
-#Time between reads
-SLEEP_TIME = 60
-
-#Log config
-LOGGER_NAME = '/home/pi/Dev/templogger/templogger.log'
+CONFIG_PATH = "config.ini"
 
 
-def set_up_logs():
-    logger = logging.getLogger(LOGGER_NAME)
+
+def set_up_logs(logfilepath):
+    logger = logging.getLogger(logfilepath)
     logger.setLevel(logging.ERROR)
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
@@ -31,29 +41,46 @@ def set_up_logs():
 
 def main():
 
-    logger = set_up_logs()
-    arduino = serial.Serial (SERIAL_PORT, SERIAL_BAUD_RATE, timeout=SERIAL_TIMEOUT)
+    config = configparser.ConfigParser()
+    try:
+        config.read(CONFIG_PATH)
+    except:
+        exit()
+
+    logger = set_up_logs(config['Logging']['LoggerName'])
+    serial_port = config['Serial']['SerialPort']
+    serial_baud_rate = int(config['Serial']['SerialBaudRate'])
+    serial_timeout = int(config['Serial']['SerialTimeout'])
+    sleep_time = int(config['Serial']['SleepTime'])
+    output_file = config['Output']['FilePath']
+    lat = float(config['OpenWeather']['Lat'])
+    lon = float(config['OpenWeather']['Lon'])
+
+    owm = pyowm.OWM(config['OpenWeather']['APIKey'], language='es')
+    arduino = serial.Serial(serial_port, serial_baud_rate, timeout=serial_timeout)
 
     while True:
         try:
             a = arduino.readline()
             hour = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             logger.debug ('Value read: ' + str(a) + ' at ' + hour)
+            obs = owm.weather_at_coords(lat, lon)
+
             try:
                 temp = float(a)
                 try:
-                    logger.debug ('Opening ' + PATH)
-                    f = open(PATH, 'a')
-                    logger.debug ('Writing ' + str(temp) + ' to ' + PATH)
-                    f.write(hour + ';' + str(temp) +'\n')
-                    logger.debug ('Closing ' + PATH)
+                    logger.debug ('Opening ' + output_file)
+                    f = open(output_file, 'a')
+                    logger.debug ('Writing ' + str(temp) + ' to ' + output_file)
+                    f.write(hour + ';' + str(temp) + ';' + str (obs.get_humidity()) + ';' + str(w.get_temperature('celsius')['temp']) + '\n')
+                    logger.debug ('Closing ' + output_file)
                     f.close()
                 except IOError:
-                    logger.error('IOError dealing with file ' + PATH)
+                    logger.error('IOError dealing with file ' + output_file)
                     
             except ValueError:
                 logger.error('ValueError converting ' + str(temp) + ' to float')
-            time.sleep(SLEEP_TIME)
+            time.sleep(sleep_time)
         except:
             logger.error ('Exception not cached, exiting...')
             arduino.close()
