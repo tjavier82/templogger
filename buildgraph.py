a839ec7d7366269092c95930285bd2721f55c43b
tjavier82 <tjavier82@gmail.com>
2019-02-25 21:24:18 +0100

    Prueba y error

--- buildgraph.py
+++ buildgraph.py
@@ -3,12 +3,15 @@
 
 import plotly.plotly as py
 import plotly.graph_objs as go
-import plotly.figure_factory as FF
+
 
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('temp.csv', header=None, parse_dates=[1], names=('date','temp'), delimiter=";")
+df = pd.read_csv(
+	'temp.csv', header=None, parse_dates=[1],
+	 names=('date','Internal Humidity', 'Internal Temperature','External Humidity', 'External Temperature'),
+	 delimiter=";")
 
 #sample_data_table = FF.create_table(df)
 #py.plot(sample_data_table, filename='sample-data-table')
@@ -26,7 +29,8 @@ df = pd.read_csv('temp.csv', header=None, parse_dates=[1], names=('date','temp')
 
 data = [go.Scatter(
 			x=df['date'],
-			y=df['temp'],
+			y1=df['Internal Temperature'],
+			y2=df['External Temperature'],
 			name = 'Temperature',
 			mode = 'lines',
 			hoverinfo = "x+y",
