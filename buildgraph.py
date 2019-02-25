1e97b254b0e16cf1be5ca372b5b6cc1f9759da5f
tjavier82 <tjavier82@gmail.com>
2019-02-25 21:39:14 +0100

    Pruebas de gráficos

--- buildgraph.py
+++ buildgraph.py
@@ -13,41 +13,11 @@ df = pd.read_csv(
 	 names=('date','Internal Humidity', 'Internal Temperature','External Humidity', 'External Temperature'),
 	 delimiter=";")
 
-#sample_data_table = FF.create_table(df)
-#py.plot(sample_data_table, filename='sample-data-table')
+trace = go.Scatter(
+	x  = df['date']
+	y1 = df['Internal Temperature']
+	y2 = df['External Temperature']
+)
 
-# #trace1 = go.Scatter(
-                    # x=df['date'], y=df['temp'], # Data
-                    # mode='lines', name='temp' # Additional options
-					# )
-#layout = go.Layout(title='Simple Plot from csv data',
-                  # plot_bgcolor='rgb(230, 230,230)')
-				   
-				 
-#fig = go.Figure(data=[trace1], layout=layout)
-#py.plot(fig, filename='temp_csv') 
-
-data = [go.Scatter(
-			x=df['date'],
-			y1=df['Internal Temperature'],
-			y2=df['External Temperature'],
-			name = 'Temperature',
-			mode = 'lines',
-			hoverinfo = "x+y",
-			connectgaps = True,
-			line = dict (
-				#color = ('rgb(205, 12, 24)'),
-				#width = 4,
-				#dash = 'dash',
-				shape = 'spline')
-				
-		)]	
-
-# Edit the layout
-layout = dict(title = 'Temperature from Arduino sensor',
-              xaxis = dict(title = 'Time'),
-              yaxis = dict(title = 'Temperature (degrees C)'),
-              )		
-
-fig = dict(data=data, layout=layout)
-py.plot(fig, filename='temp')
+data = [trace]
+py.iplot(data, filename='Temp evolution')
