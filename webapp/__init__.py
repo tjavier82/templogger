d54c6972ea614b322d7c8d978917e6f1834aab67
tjavier82 <tjavier82@gmail.com>
2019-03-02 15:33:30 +0100

    Se incorporan más funcionalidades a flask

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,29 @@
 
 
+import os
 from flask import Flask
 
-webapp = Flask(__name__)
 
 from webapp import routes
 
+def create_webapp(test_config=None):
+    # create and configure the app
+    webapp = Flask(__name__, instance_relative_config=True)
+    webapp.config.from_mapping(
+        SECRET_KEY='dev'
+        )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        webapp.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        webapp.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(webapp.instance_path)
+    except OSError:
+        pass
+
+    return webapp
